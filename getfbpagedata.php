96075c86cdf9fc55a469538fb73b4adfd4cb33c8
hasi-era <webkit.hasitha@gmail.com>
2015-06-12 16:35:41 +0530

    Update getfbpagedata.php

--- getfbpagedata.php
+++ getfbpagedata.php
@@ -1,6 +1,6 @@
 <?php
     /**
-    * This is the class useed for get the updates of pages
+    * This is the class useed for get the updates of pages using Facebook graph API
     *
     * Long description for class (if any)...
     * @author     Hasitha Eranga
@@ -15,14 +15,15 @@ class GetfbPageData{
         //use one og below method to get data from FB page
         
         //get data using CURL request
-        //$pageUpdateData = $this->curl_get_contents("https://graph.facebook.com/v2.3/PrimaKottumeeLK/photos?type=uploaded&access_token=CAACEdEose0cBAPX8PW7XJvZB2eaOQPCDwql2L2Kdyd76G9YM09ChmG6inYmVwEDyrN5WTBXlRZB1NvHrtZBYEN13bGWbupD7ATqsiXAyouv3bHEL9qe7sIew0uLxxeaoocXzgXdF7BZAUxLXRd4FKDj5sjrW6zBwn0BtGVnbPyiM9Rst7v11eF83031lZCzP4ruvIX86R5wZDZD");
+        //$pageUpdateData = $this->curl_get_contents("https://graph.facebook.com/v2.3/Your-Page-Name/photos?type=uploaded&access_token=Your-Access-Token");
         
         //get data using file_get_contents php function
-        $pageUpdateData = file_get_contents("https://graph.facebook.com/v2.3/PrimaKottumeeLK/photos?type=uploaded&access_token=CAACEdEose0cBAPX8PW7XJvZB2eaOQPCDwql2L2Kdyd76G9YM09ChmG6inYmVwEDyrN5WTBXlRZB1NvHrtZBYEN13bGWbupD7ATqsiXAyouv3bHEL9qe7sIew0uLxxeaoocXzgXdF7BZAUxLXRd4FKDj5sjrW6zBwn0BtGVnbPyiM9Rst7v11eF83031lZCzP4ruvIX86R5wZDZD", true);
+        $pageUpdateData = file_get_contents("https://graph.facebook.com/v2.3/Your-Page-Name/photos?type=uploaded&access_token=Your-Access-Token", true);
         
         return $pageUpdateData;
     }
     
+    //Function to call CURL
     function curl_get_contents($url)
     {
       $curl = curl_init($url);
@@ -36,9 +37,9 @@ class GetfbPageData{
     }
     
 }
-
+    //Creating The Object
     $curls = new Getfbpageimages;
-    
+    //Call the Method 
     $std = $curls->getPageUpdate();
     
     $json_feed_object = json_decode($std);
@@ -46,9 +47,10 @@ class GetfbPageData{
     foreach ( $json_feed_object->data as $key =>$entry )
     {
         echo '<p style="font-size:20px;">'.$entry->name.'</p>';
+        //this will echo the main source image(no resizing)
         echo '<img src="'.$entry->source.'">';
         echo '<a href='.$entry->link.'>aa</a>';
-        //break;//Uncomment this to get letest post from the page
+        //break;//Uncomment this to get only latest post from the page
     }
 
 
